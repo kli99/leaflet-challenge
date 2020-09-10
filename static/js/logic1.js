42f4b819fc603130b3fa1e29d2c1a922712e706e
KEFAN LI <1350-kefan.li99@users.noreply.columbia.bootcampcontent.com>
2020-09-10 02:42:29 -0400

    New

--- static/js/logic1.js
+++ static/js/logic1.js
@@ -26,7 +26,7 @@ function createColor(magnitude) {
     return color;
 };
 
-// create markers features for the map
+// create markers features for the overlay map
 function createFeatures(earthquakeData) {
 
     // Define a function we want to run once for each feature in the features array
@@ -65,52 +65,137 @@ function createFeatures(earthquakeData) {
         onEachFeature: onEachFeature
     });
 
+    // function createLine(feature, latlng) {
+    //     return L.polyline(latlng);
+    // }
+
+
+
+
+    // function createNew(feature) {
+    //     var stylesInfo = {
+    //         stroke: true,
+    //         color: "tomato",
+    //         weight: 1
+    //     }
+    //     return stylesInfo;
+    // }
+    // var faultlines = L.geoJSON(earthquakeData, {
+    //     pointToLayer: createLine,
+    //     style: createNew,
+    //     onEachFeature: onEachFeature
+    // });
+
 
     // Sending our earthquakes layer to the createMap function
     createMap(earthquakes);
 
 };
 
+// create a red polyline from an arrays of LatLng points
+var polyline = L.polyline(latlng, { color: 'red' }).addTo(map);
+
+// zoom the map to the polyline
+map.fitBounds(polyline.getBounds());
+
+// // create markers features for the map
+// function createFeatures(earthquakeData) {
+
+//     // Define a function we want to run once for each feature in the features array
+//     // Give each feature a popup describing the place and time of the earthquake
+
+
+//     function onEachFeature(feature, layer) {
+//         layer.bindPopup("<h3>" + feature.properties.place +
+//             "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
+//     };
+
+//     // Create a GeoJSON layer containing the features array on the earthquakeData object
+//     // Run the onEachFeature function once for each piece of data in the array
+//     function createMarker(feature, latlng) {
+//         return L.circleMarker(latlng);
+//     };
+
+//     // style function with calling straight from API
+//     function createStyle(feature) {
+//         var styleInfo = {
+//             fillColor: createColor(feature.properties.mag),
+//             fillopacity: 1,
+//             stroke: true,
+//             color: "black",
+//             weight: 1,
+//             radius: feature.properties.mag * 8
+//         }
+
+//         return styleInfo;
+//     };
+
+//     // geojson the functions into one
+//     var earthquakes = L.geoJSON(earthquakeData, {
+//         pointToLayer: createMarker,
+//         style: createStyle,
+//         onEachFeature: onEachFeature
+//     });
+
+
+//     // Sending our earthquakes layer to the createMap function
+//     createMap(earthquakes);
+
+// };
+
+
+
+
 // var myMap = L.map("map").setView([37.09, -95.71], 5);
 function createMap(earthquakes) {
 
 
-    // Create base layers
-    var statelite = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-        attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    // Create base layers by creating new style in mapbox studio
+    var statellite = L.tileLayer('https://api.mapbox.com/styles/v1/kli99/ckewep1ef0jbb19osoltavxxm/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoia2xpOTkiLCJhIjoiY2tlZGxtNDFqMGk5dDM1azlpMHVwbnk2YSJ9.PYjXx4q4ceRv1OjQZCNKKw', {
+
+        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
         tileSize: 512,
         maxZoom: 18,
         zoomOffset: -1,
-        id: "mapbox/streets-v11",
-        accessToken: API_KEY
     });
 
-    var grayscale = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    var grayscale = L.tileLayer("https://api.mapbox.com/styles/v1/kli99/ckewfjx7q03cl19nu2zmtgmmg/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoia2xpOTkiLCJhIjoiY2tlZGxtNDFqMGk5dDM1azlpMHVwbnk2YSJ9.PYjXx4q4ceRv1OjQZCNKKw", {
         attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+        tileSize: 512,
         maxZoom: 18,
-        id: "dark-v10",
-        accessToken: API_KEY
+
     });
 
-    var outdoors = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    var outdoors = L.tileLayer("https://api.mapbox.com/styles/v1/kli99/ckewevb490cqx19psxlblpa5b/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoia2xpOTkiLCJhIjoiY2tlZGxtNDFqMGk5dDM1azlpMHVwbnk2YSJ9.PYjXx4q4ceRv1OjQZCNKKw", {
         attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+        tileSize: 512,
         maxZoom: 18,
-        id: "dark-v10",
-        accessToken: API_KEY
     });
 
+    //var faultlines = L.layergroup
 
     // Define a baseMaps object to hold our base layers
     var baseMaps = {
-        "Statelite": statelite,
+
+        "Statellite": statellite,
         "Grayscale": grayscale,
         "Outdoors": outdoors
     };
 
+
+
+    // // create a red polyline from an arrays of LatLng points
+    // var polyline = L.polyline(latlng, { color: 'red' }).addTo(map);
+
+    // // zoom the map to the polyline
+    // map.fitBounds(polyline.getBounds());
+
+
     // Create overlay object to hold our overlay layer
     var overlayMaps = {
 
-        Earthquakes: earthquakes
+        "Fault Lines": polyline,
+        "Earthquakes": earthquakes
     };
 
     // Create our map, giving it the streetmap and earthquakes layers to display on load
@@ -121,7 +206,7 @@ function createMap(earthquakes) {
         ],
         zoom: 5,
 
-        layers: [statelite, earthquakes]
+        layers: [statellite, earthquakes]
     });
 
     // Create a layer control
